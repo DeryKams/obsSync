60496bfe74c914e66adb6f3106e2b04474d94a98
DeryKams <derykams@gmail.com>
2025-10-31 14:37:54 +1000

    adding installing rclone

--- rclonService.sh
+++ rclonService.sh
@@ -1,15 +1,32 @@
-if pacman -Qi rclone >/dev/null 2>&1; then
+#!/bin/bash
+
+CycleChecking=2
+
+# -ge - greater or equal, то есть больше или равно
+while [ $CycleChecking -ge 0 ]; do
     
-    echo "rclone Installed"
-    CONF_PATH="$(rclone config file | tail -n 1)"
-    
-    if [ -n $CONF_PATH ]; then
-        echo "Path to config rclone: $CONF_PATH"
+    if pacman -Qi rclone >/dev/null 2>&1; then
+        
+        echo "rclone Installed"
+        CONF_PATH="$(rclone config file | tail -n 1)"
+        
+        # -n - non empty - то есть если строка не пустая
+        if [ -n "$CONF_PATH" ]; then
+            echo "Path to config rclone: $CONF_PATH"
+        else
+            echo "config is empty $CONF_PATH"
+        fi
+        
     else
-        echo "config is empty $CONF_PATH"
+        
+        echo "rclone dont istalled"
+        echo "installing rclone"
+        
+        sudo pacman -Syu rclone
+        
     fi
-else
-    echo "rclone dont istalled"
     
-    exit 1
-fi
+    #$(()) - арифметическая подстановка в bash
+    CycleChecking=$((CycleChecking - 1))
+    
+done
