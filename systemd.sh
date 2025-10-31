698c18b61b817400e6f31ade06c3833479207422
DeryKams <derykams@gmail.com>
2025-10-31 17:53:57 +1000

    Checking if the service is running

--- systemd.sh
+++ systemd.sh
@@ -2,14 +2,14 @@
 #${} - подстановка переменной
 SCRIPT_PATH="$(readlink -f "$0")"
 # Проверяем существует ли такая директория для systemd сервисов пользователя
-if [ -d ~/.config/systemd/$USER ]; then
+if [ -d ~/.config/systemd/user ]; then
     echo "User systemd directory exists"
 else
-    mkdir -p ~/.config/systemd/$USER
+    mkdir -p ~/.config/systemd/user
     echo "User systemd directory was created"
 fi
 
-# Проверяем, созад ли user systemd service для синхронизации Obsidian
+# Проверяем, создан ли user systemd service для синхронизации Obsidian
 
 if [ -f ~/.config/systemd/user/obsSync.service ]; then
 
@@ -20,16 +20,35 @@ else
 echo " Creating synchronization service in user systemd..."
 
 cat > ~/.config/systemd/user/obsSync.service << EOF
+[Unit]
+Description= Скрипт для синхронизации Obsidian с внешним хранилищем. Настраивается посредством rclone.
+After=network.target
+Documentation=https://github.com/DeryKams/obsSync
+
+[Service]
+Type=simple
+ExecStart=$SCRIPT_PATH
+Restart=always
+RestartSec=10
+
+NoNewPrivileges=yes
+ProtectSystem=strict
+ProtectHome=read-only
+ReadWritePaths=%h/obsidian /tmp
+
+MemoryLimit=100M
+CPUQuota=10%
+
+[Install]
+WantedBy=default.target
+EOF
 
 #[Unit] - секция для описания и зависимостей
 #Description - человекочитаемое описание сервиса
 #After=network.target - запускать после загрузки сети
 #network.target - системная точка, которая достигается после запуска сети
 #Documentations
-[Unit]
-Description= Скрипт для синхронизации Obsidian с внешним хранилищем. Настраевтся посредством rclon
-After=network.target
-Documentation=https://github.com/DeryKams/obsSync
+
 
 #[Service] - настройки выполнения
 #type=simple - тип сервиса
@@ -45,30 +64,32 @@ Documentation=https://github.com/DeryKams/obsSync
 # no - не перезапускать
 # RestartSec = 10 - ждать перед запуском 10 секунд
 
-[Service]
-Type=simple
-ExecStart=$SCRIPT_PATH
-Restart=always
-RestartSec=10
-
 #Ограничения по безопасности
 
 #Запрещаем процессу повышать привелегии
-NoNewPrivileges=yes
+# NoNewPrivileges=yes
 #Защита системных файлов - доступны только для чтения. Запрещена запись в системные директории
-ProtectSystem=strict
+# ProtectSystem=strict
 #Домашняя директория доступна только для чтения
-ProtectHome=read-only
+# ProtectHome=read-only
 # Исключения для записи и чтения
-ReadWritePaths=%h/obsidian /tmp
+# ReadWritePaths=%h/obsidian /tmp
 
 #Ограничения по памяти и процессору
-MemoryLimit=500M
-CPUQuota=10%
+# MemoryLimit=500M
+# CPUQuota=10%
 
-[Install]
-WantedBy=default.target
-EOF
+fi
 
 
+fi
+
+# Проверяем загружена ли служба в systemd
+if systemctl --user list-unit-files | grep -q obsSync.service; then
+    echo "Service is loaded in systemd"
+else
+
+echo "The startup service needs to be started."
+systemctl --user daemon-reload
+systemctl --user enable --now obsSync.service
 fi
